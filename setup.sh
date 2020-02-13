e1344e7d02a6535b99de460e03e7b27fc51583df
VictorSungminYou <43061525+VictorSungminYou@users.noreply.github.com>
2020-02-13 13:11:26 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ -z "$TF" ]
-then
-    TF=tensorflow
-else
-    TF=tensorflow-gpu
-fi
-
-
 pip install -r requirements.txt
-pip install --upgrade $TF==1.8.0
+pip install --upgrade tensorflow-gpu==1.8.0
 
